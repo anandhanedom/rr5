bdbd7e49ea03df2ffbc2c215d384e455a751ce70
Anandhan <anandhanedom@gmail.com>
2021-10-05 23:36:07 +0530

    fix: name

--- lesson12.js
+++ lesson12.js
@@ -1,5 +1,5 @@
 const { search } = useLocation();
-const searcParams = new URLSearchParams(search);
+const searchParams = new URLSearchParams(search);
 
 const name = searchParams.get("name");
 const age = searchParams.get("age");
